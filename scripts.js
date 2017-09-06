e81216b93b4a814c28ca2623150c93657cbc4355
Wil Roberts <war7699@uga.edu>
2017-09-06 14:28:24 -0400

    Update
    Finished basic line drawing algorithm. Re-wrote part it to take parameters so it can be called with input from other functions.
    
    Wrote a function to generate X number of lines.

--- scripts.js
+++ scripts.js
@@ -3,16 +3,33 @@ var context = canvas.getContext('2d');
 context.fillStyle = "black";
 
 window.onload = function(){
-	document.getElementById('drawLine').setAttribute('onclick', 'drawLine()');
-	
+	document.getElementById('drawLine').setAttribute('onclick', 'getCoordinates()');
+	document.getElementById('generateLines').setAttribute('onclick', 'generateLines()');
 }
 
-function drawLine(){
-	context.beginPath();
+function getCoordinates(){
 	var x0 = parseInt(document.getElementById('xValue0').value, 10);
 	var x1 = parseInt(document.getElementById('xValue1').value, 10);
 	var y0 = parseInt(document.getElementById('yValue0').value, 10);
 	var y1 = parseInt(document.getElementById('yValue1').value, 10);
+	drawLine(x0,x1,y0,y1);
+}
+
+function generateLines(){
+	var iterations = parseInt(document.getElementById('iterations').value, 10);
+	console.log(iterations);
+	for(var i = 0; i < iterations; i++){
+		var x0 = Math.floor(Math.random() * 400);
+		var y0 = Math.floor(Math.random() * 400);
+		var x1 = Math.floor(Math.random() * 400);
+		var y1 = Math.floor(Math.random() * 400);
+		console.log("(" + x0 + "," + y0 + ")(" + x1 + "," + y1 + ")");
+		drawLine(x0,x1,y0,y1);
+	}
+}
+
+function drawLine(x0,x1,y0,y1){
+	context.beginPath();
 	var deltaX = x1 - x0;
 	var deltaY = y1 - y0;
 	var m = deltaY / deltaX;
@@ -20,7 +37,7 @@ function drawLine(){
 	var m2 = deltaX / deltaY;
 	var looper;
 	var change;
-	if(deltaY > deltaX){
+	if(Math.abs(deltaY) > Math.abs(deltaX)){
 		looper = Math.abs(deltaY);
 		if(deltaY < 0){
 			change = -1;
